17ec3608b136557e981b8066198464976c712461
Ross Thomson <rthomson412@gmail.com>
2022-07-15 15:11:10 -0400

    refavtor

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import Footer from "./components/Footer";
 
 function App() {
   const [pages] = useState([
-      {name: "about me"},
-      {name: "portfolio"},
-      {name: "contact"},
-      {name: "resume"}
+      { name: "about me" },
+      { name: "portfolio" },
+      { name: "contact" },
+      { name: "resume" }
   ]);
 
   const [currentPage, setCurrentPage] = useState(pages[0]);
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -11,31 +11,31 @@ function Portfolio() {
       repo: "https://github.com/rthomson412/upStarter"
     },
     {
-      name: 'Much Better Dictionary',
+      name: 'much-better-dictionary',
       description: 'Search reccomendations, speech recognition and APIs for definitions and pictures.',
       link: "https://muchbetterdictionary.github.io/MuchBetterDictionary/",
       repo: "https://github.com/MuchBetterDictionary/MuchBetterDictionary"
     },
     {
-      name: 'Developer Code Quiz',
+      name: 'code-quiz',
       description: 'Test your dev knowledge with this simple format quiz. Uses Html,  Css and Javascript.',
       link: "https://rthomson412.github.io/Code-Quiz/",
       repo: "https://github.com/rthomson412/Code-Quiz"
     },
     {
-      name: 'Workday Scheduler',
+      name: 'workday-scheduler',
       description: 'Organize the events of your day and see what events have passed and are upcoming.',
       link: "https://rthomson412.github.io/Work-Day-Scheduler/",
       repo: "https://github.com/rthomson412/Work-Day-Scheduler"
     },
     {
-      name: 'Weather Dashboard',
+      name: 'weather-dashboard',
       description: 'Search and store your cities and retrieve a 5 day forecast as well as current conditions.',
       link: "https://rthomson412.github.io/weatherDash/",
       repo: "https://github.com/rthomson412/weatherDash"
     },
     {
-      name: 'Password Generator',
+      name: 'password-generator',
       description: 'Quick and simple application to generate a random password, with the character types you choose.',
       link: "https://rthomson412.github.io/PasswordGenerator/",
       repo: "https://github.com/rthomson412/PasswordGenerator"
