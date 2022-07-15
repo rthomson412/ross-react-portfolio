e9f47533519147c6943a403343bf3267f2aa74a9
Ross Thomson <rthomson412@gmail.com>
2022-07-15 12:54:00 -0400

    header1

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import coverImage from '../../assets/cover/cover.jpg';
+
+function Header(props) {
+
+  return (
+    <header className="flex-row space-between px-1">
+      <h1>Ross Thomson</h1>
+      <img src={coverImage} alt="psychadelic"></img>
+      {props.children}
+    </header>
+  );
+}
+
+export default Header;
