075324204f9497362c8451a5ae653b78a3e2e593
Ross Thomson <rthomson412@gmail.com>
2022-07-15 12:40:44 -0400

    form js

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,7 +5,7 @@ function About() {
     <section className="my-5">
       <div className="my-2">
         <div className="profile-img my-5">
-        <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i>
+        <i className="fas fa-user-circle" style={{ fontSize: "124px" }}></i>
         </div>
         <p></p>
         <p></p>
