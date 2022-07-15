a5276b90c0b7f6dc851bb2dd86af754bd6cdf79e
Ross Thomson <rthomson412@gmail.com>
2022-07-15 12:49:48 -0400

    footer1

---
+++ src/components/Footer/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Footer() {
+
+  const icons = [
+    {
+      name: "fab fa-github",
+      link: "https://github.com/rthomson412"
+    },
+    {
+      name: "fab fa-envelope",
+      link: "mailto:rthomson412@gmail.com"
+    },
+    {
+      name: "fab fa-youtube",
+      link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    }
+  ]
+
+  
