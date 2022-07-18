b15dd56c165b60545a9ec24fb03b477b34d28d52
Ross Thomson <rthomson412@gmail.com>
2022-07-17 20:39:45 -0400

    resume page

---
+++ src/components/Resume/index.js
@@ -0,0 +1,108 @@
+import React from 'react';
+
+function Resume() {
+    return (
+
+        <body id="top">
+            <div>
+                <p>
+                    Click here to view my <a href="https://www.linkedin.com/in/ross-thomson-a6081122a/">resume.</a>
+                </p>
+                <div id="name">
+                    <h1>Ross Thomson{'\n'}</h1>
+                    <h2>Software developer{'\n'}</h2>
+                </div>
+                <div class="clear"></div>
+
+
+                <div>
+                    <section>
+                        <article>
+                            <div class="sectionTitle">
+                                <h1>Personal Profile</h1>
+                            </div>
+                            <div class="sectionContent">
+                                <p>Across the centuries hydrogen atoms of brilliant syntheses vastness is bearable only through love the only home we've ever known made in the interiors of collapsing stars? Intelligent beings ship of the imagination are creatures of the cosmos take root and flourish astonishment descended from astronomers. Not a sunrise but a galaxyrise brain is the seed of intelligence vanquish the impossible the ash of stellar alchemy hearts of the stars finite but unbounded and billions upon billions upon billions upon billions upon billions upon billions upon billions.</p>
+                            </div>
+                        </article>
+                        <div class="clear"></div>
+                    </section>
+
+
+                    <section>
+                        <div class="sectionTitle">
+                            <h1>Work Experience</h1>
+                        </div>
+
+                        <div class="sectionContent">
+                            <article>
+                                <h2>Job Title at Company</h2>
+                                <p class="subDetails">March 2020 - Present</p>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                            </article>
+
+                            <article>
+                                <h2>Job Title at Company</h2>
+                                <p class="subDetails">May 2016 - March 2020</p>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                            </article>
+
+                            <article>
+                                <h2>Job Title at Company</h2>
+                                <p class="subDetails">October 2011 - December 2016</p>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                            </article>
+                        </div>
+                        <div class="clear"></div>
+                    </section>
+
+
+                    <section>
+                        <div class="sectionTitle">
+                            <h1>Key Skills</h1>
+                        </div>
+
+                        <div class="sectionContent">
+                            <ul class="keySkills">
+                                <li>HTML, CSS, Javascript</li>
+                                <li>jQuery, Responsive design</li>
+                                <li>Bootstrap, Bulma</li>
+                                <li>APIs</li>
+                                <li>Node, Express</li>
+                                <li>MySQL, Sequelize, NoSQL</li>
+                                <li>MERN stack</li>
+                                <li>React, GraphQL</li>
+                            </ul>
+                        </div>
+                        <div class="clear"></div>
+                    </section>
+
+
+                    <section>
+                        <div class="sectionTitle">
+                            <h1>Education</h1>
+                        </div>
+
+                        <div class="sectionContent">
+                            <article>
+                                <h2>College/University</h2>
+                                <p class="subDetails">Qualification</p>
+                                <p>Across the centuries hydrogen atoms of brilliant syntheses vastness is bearable only through love the only home we've ever known made in the interiors of collapsing stars? Intelligent beings ship of the imagination are creatures of the cosmos take root and flourish astonishment descended from astronomers. Not a sunrise but a galaxyrise brain is the seed of intelligence vanquish the impossible the ash of stellar alchemy hearts of the stars finite but unbounded and billions upon billions upon billions upon billions upon billions upon billions upon billions.</p>
+                            </article>
+
+                            <article>
+                                <h2>College/University</h2>
+                                <p class="subDetails">Qualification</p>
+                                <p>Across the centuries hydrogen atoms of brilliant syntheses vastness is bearable only through love the only home we've ever known made in the interiors of collapsing stars? Intelligent beings ship of the imagination are creatures of the cosmos take root and flourish astonishment descended from astronomers. Not a sunrise but a galaxyrise brain is the seed of intelligence vanquish the impossible the ash of stellar alchemy hearts of the stars finite but unbounded and billions upon billions upon billions upon billions upon billions upon billions upon billions.</p>
+                            </article>
+                        </div>
+                        <div class="clear"></div>
+                    </section>
+
+                </div>
+            </div>
+        </body>
+    );
+}
+
+export default Resume;
